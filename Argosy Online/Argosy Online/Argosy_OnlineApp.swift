c07f0ce31deb3463be89c56d9b02cab2053f6b1b
Dias <dias.atudinov@gmail.com>
2025-05-15 18:23:40 +0500

    app setup

---
+++ Argosy Online/Argosy Online/Argosy_OnlineApp.swift
@@ -0,0 +1,17 @@
+//
+//  Argosy_OnlineApp.swift
+//  Argosy Online
+//
+//  Created by Dias Atudinov on 15.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct Argosy_OnlineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
